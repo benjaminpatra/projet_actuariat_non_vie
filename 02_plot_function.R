ccddbc6e276f1b9730ddac3133e7401cc75b1faf
emaucuer <96245397+emaucuer@users.noreply.github.com>
2021-12-23 13:37:28 +0100

    Ajout d'un as.factor pour la couleur

--- 02_plot_function.R
+++ 02_plot_function.R
@@ -7,29 +7,31 @@ fun_plot <- function(data,var1,var2){
               
               data_plot2 <<- data %>% 
                 group_by(across(var1)) %>% 
-                summarise(n_DriverAge = n()) %>% 
+                summarise(n_var1 = n()) %>% 
                 ungroup()
               
               print(as.character(as.name(var1)))
               data_plot <<- merge(data_plot1, data_plot2, by = var1)
               
-              data_plot$freq = data_plot$n/data_plot$n_DriverAge
+              data_plot$freq = data_plot$n/data_plot$n_var1
               
               data_plot <<- data_plot
   
-              plot_DriverAge <- plot_ly(data_plot) %>%
-                add_trace(x =as.formula(paste0("~", var1)), y =~ n, type = 'bar', color =as.formula(paste0("~", var2))) %>% 
+              plot <- plot_ly(data_plot) %>%
+                add_trace(x =as.formula(paste0("~", var1)), y =~ n, 
+                          type = 'bar', color = as.formula(paste0("~","as.factor(", var2,")"))) %>% 
                 layout(barmode = 'stack',
                        yaxis = list(title = "Absolute frequency"),
-                       title = "Claim Number by Driver Age")
-              print(plot_DriverAge)
+                       title = paste0(var2," by ", var1))
+              print(plot)
               
-              plot_DriverAge_100 <- plot_ly(data_plot) %>%
-                add_trace(x =as.formula(paste0("~", var1)), y =~ freq, type = 'bar', color =as.formula(paste0("~", var2)), opacity = 0.8) %>% 
+              plot_100 <- plot_ly(data_plot) %>%
+                add_trace(x = as.formula(paste0("~", var1)), y =~ freq, 
+                          type = 'bar', color = as.formula(paste0("~","as.factor(", var2,")")), opacity = 0.8) %>% 
                 layout(barmode = 'stack',
                        yaxis = list(title = "proportion"),
-                       title = "Claim Number by Driver Age")
-              print(plot_DriverAge_100)
+                       title = paste0(var2," by ", var1))
+              print(plot_100)
               
               
 }
@@ -37,6 +39,6 @@ fun_plot <- function(data,var1,var2){
 
 
 fun_plot(data = freMTPLfreq,var1 = "CarAge",var2 = "ClaimNb")
-fun_plot(data = freMTPLfreq,var1 = "Driver",var2 = "ClaimNb")
+fun_plot(data = freMTPLfreq,var1 = "DriverAge",var2 = "ClaimNb")
 
 
