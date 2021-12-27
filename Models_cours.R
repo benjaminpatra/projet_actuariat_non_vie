b0eefa18ae5d63246eaeed0db0b9bffe8096f3ae
emaucuer <96245397+emaucuer@users.noreply.github.com>
2021-12-27 18:15:42 +0100

    Ajout de nouveaux plot + modif modeles sev

--- Models_cours.R
+++ Models_cours.R
@@ -4,6 +4,8 @@
 ##                                    ##
 ##------------------------------------##
 
+setwd("~/Documents/Education/ENSAE/3A/S1/Actuariat non vie")
+
 # Libraries --------------------
 require("xts")
 #install.packages("CASdatasets", repos = "http://dutangc.perso.math.cnrs.fr/RRepository/pub/", type="source")
@@ -16,7 +18,9 @@ library(mvabund)
 library(pscl)
 library(plotly)
 library(stats)
+library(modi)
 
+source("02_plot_function.R")
 
 # Data --------------------
 data(freMTPLfreq)
@@ -26,81 +30,29 @@ data(freMTPLsev)
 freMTPLfreq$DriverAgeG <- cut(freMTPLfreq$DriverAge, c(17, 2:8*10, 100))
 freMTPLfreq$CarAgeG <- cut(freMTPLfreq$CarAge, c(-1, 0:1*5, 100))
 freMTPLfreq$LDensity <- log(freMTPLfreq$Density)
-freMTPLsev$LClaimAmount <- log(freMTPL$ClaimAmount)
+freMTPLsev$LClaimAmount <- log(freMTPLsev$ClaimAmount)
 
-freMTPL <- merge(freMTPLfreq, freMTPLsev, by = "PolicyID", all =T)
-
+freMTPL <- merge(freMTPLfreq, freMTPLsev, by = "PolicyID", all = T) # base avec doublon de policy id
+freMTPLsev <- merge(freMTPLsev, freMTPLfreq, by = "PolicyID", al.x = T) # base des claims
 
 # Régression pour la fréquence ----------------
 summary(freMTPLfreq)
 
 ### Stats desc ----
-# Car Age
-data_plot1 <- freMTPLfreq %>% 
-  group_by(CarAge,ClaimNb) %>% 
-  summarise(n = n()) %>% 
-  ungroup()
+# 3.3 Car Age
+fun_plot(freMTPLfreq, "CarAge", "ClaimNb")
 
-data_plot2 <- freMTPLfreq %>% 
-  group_by(CarAge) %>% 
-  summarise(n_carAge = n()) %>% 
-  ungroup()
+# 3.4 Driver Age
+fun_plot(freMTPLfreq, "DriverAge", "ClaimNb")
 
-data_plot <- merge(data_plot1, data_plot2, by = "CarAge")
-data_plot$freq = data_plot$n/data_plot$n_carAge
+# 3.5 Power
+fun_plot(freMTPLfreq, "Power", "ClaimNb")
 
-plot_CarAge <- plot_ly(data_plot) %>%
-  add_trace(x =~ CarAge, y =~ n, type = 'bar', color =~ as.factor(ClaimNb), opacity = 0.8) %>% 
-  layout(barmode = 'stack',
-         yaxis = list(title = "Absolute frquency"),
-         title = "Claim Number by Car Age")
-plot_CarAge
-
-plot_CarAge_100 <- plot_ly(data_plot) %>%
-  add_trace(x =~ CarAge, y =~ freq, type = 'bar', color =~ as.factor(ClaimNb), opacity = 0.8) %>% 
-  layout(barmode = 'stack',
-         yaxis = list(title = "proportion"),
-         title = "Claim Number by Car Age")
-plot_CarAge_100
-
-# Driver Age
-data_plot1 <- freMTPLfreq %>% 
-  group_by(DriverAge,ClaimNb) %>% 
-  summarise(n = n()) %>% 
-  ungroup()
-
-data_plot2 <- freMTPLfreq %>% 
-  group_by(DriverAge) %>% 
-  summarise(n_DriverAge = n()) %>% 
-  ungroup()
-
-data_plot <- merge(data_plot1, data_plot2, by = "DriverAge")
-data_plot$freq = data_plot$n/data_plot$n_DriverAge
-
-plot_DriverAge <- plot_ly(data_plot) %>%
-  add_trace(x =~ DriverAge, y =~ n, type = 'bar', color =~ as.factor(ClaimNb)) %>% 
-  layout(barmode = 'stack',
-         yaxis = list(title = "Absolute frquency"),
-         title = "Claim Number by Driver Age")
-plot_DriverAge
-
-plot_DriverAge_100 <- plot_ly(data_plot) %>%
-  add_trace(x =~ DriverAge, y =~ freq, type = 'bar', color =~ as.factor(ClaimNb), opacity = 0.8) %>% 
-  layout(barmode = 'stack',
-         yaxis = list(title = "proportion"),
-         title = "Claim Number by Driver Age")
-plot_DriverAge_100
-
-
-# Mean Variance Plot
-plot(freMTPLfreq %>% group_by(Power) %>%
-       summarise(m = weighted.mean(ClaimNb, Exposure),
-                 v = weighted.var(ClaimNb, Exposure)))
-
-plot(freMTPLfreq %>% group_by(Region) %>%
-       summarise(m = mean(ClaimNb),
-                 v = var(ClaimNb)))
-
+# 3.8 – Relation moyenne / variance du nombre de sinistres
+mean_variance(freMTPLfreq, 'DriverAge', 'ClaimNb')
+mean_variance(freMTPLfreq, 'CarAge', 'ClaimNb')
+mean_variance(freMTPLfreq, 'Power', 'ClaimNb')
+mean_variance(freMTPLfreq, 'Region', 'ClaimNb')
 
 
 ### Modele poisson ----
@@ -109,10 +61,7 @@ fpois2 <- glm(ClaimNb ~ Power+CarAgeG+DriverAgeG+Brand
               family=poisson("log"), data=freMTPLfreq)
 summary(fpois2)
 #xtable(coef(summary(fpois2)), digits=3) 
-
-plot(fpois2)
-plot(fitted(fpois2), resid(fpois2), 
-     ylab="Residuals", xlab="fitted") 
+plotgroupresiduals(fpois2) #, trim = F, m =1
 
 
 ### Modele Poisson sur dispersée (quasi poisson) ----
@@ -121,6 +70,7 @@ fqpois3 <- glm(ClaimNb ~ Power+CarAgeG+DriverAgeG+Brand
                family=quasipoisson("log"), data=freMTPLfreq)
 summary(fqpois3)
 summary(fqpois3)$dispersion #phi
+plotgroupresiduals(fqpois3) #, trim = F, m =1
 
 
 ### Modele binomiale negative ----
@@ -139,15 +89,15 @@ summary(fnb5)
 
 
 ### Zero inflaté ----
-fzip_logit <- zeroinfl(ClaimNb ~ Power+CarAgeG+DriverAgeG+Brand
-               +Gas+Region+Density+offset(log(Exposure)),
-               data=freMTPLfreq, dist = "poisson", link = "logit")
-summary(fzip_logit)
-
-fzip_probit <- zeroinfl(ClaimNb ~ Power+CarAgeG+DriverAgeG+Brand
-                       +Gas+Region+Density+offset(log(Exposure)),
-                       data=freMTPLfreq, dist = "poisson", link = "probit")
-summary(fzip_probit)
+# fzip_logit <- zeroinfl(ClaimNb ~ Power+CarAgeG+DriverAgeG+Brand
+#                +Gas+Region+LDensity+offset(log(Exposure)),
+#                data=freMTPLfreq, dist = "poisson", link = "logit")
+# summary(fzip_logit)
+# 
+# fzip_probit <- zeroinfl(ClaimNb ~ Power+CarAgeG+DriverAgeG+Brand
+#                        +Gas+Region+LDensity+offset(log(Exposure)),
+#                        data=freMTPLfreq, dist = "poisson", link = "probit")
+# summary(fzip_probit)
 
 
 
@@ -158,17 +108,23 @@ freMTPL_filtered <- freMTPL %>% filter(ClaimAmount<=1125 | ClaimAmount>=1221)
 summary(freMTPL)
 summary(freMTPL_filtered)
 
-# Fonction de répartition empirique
+# 4.2 - Fonction de répartition empirique
 plot(ecdf(freMTPL_filtered$ClaimAmount), xlim = c(0, 10000))
 plot(ecdf(freMTPL_filtered$LClaimAmount), xlim = c(0, 15))
 
-# box plot ClaimAmount
+# 4.3 – Boxplot des montants et des log-montants
 ggplot(freMTPL_filtered, aes(x="1", y=ClaimAmount)) + 
   geom_boxplot()
 ggplot(freMTPL_filtered, aes(x="1", y=LClaimAmount)) + 
   geom_boxplot()
 
-# Boxplot bivariée ClaimAmount x Autre variable
+# 4.4 - Boxplot des montants de sinistre sur les caract ́eristiques du v ́ehicule
+amountvsnb(freMTPLsev$ClaimAmount)
+amountvsnb_grouped(freMTPLsev, "DriverAgeG", "ClaimAmount")
+amountvsnb_grouped(freMTPLsev, "Power", "ClaimAmount")
+
+
+# 4.5 et 4.6 - Boxplot bivariée ClaimAmount x Autre variable
 ggplot(freMTPL_filtered, aes(x=as.factor(DriverAge), y=ClaimAmount, fill=as.factor(DriverAge))) + 
   geom_boxplot(outlier.shape = NA) +
   coord_cartesian(ylim = quantile(freMTPL_filtered$ClaimAmount, c(0.1, 0.9)))
@@ -185,24 +141,27 @@ ggplot(freMTPL_filtered, aes(x=as.factor(Power), y=ClaimAmount)) +
   geom_boxplot(outlier.shape = NA) +
   coord_cartesian(ylim = quantile(freMTPL_filtered$ClaimAmount, c(0.1, 0.9)))
 
-
+fun_boxplot(freMTPL_filtered, "Region", "ClaimAmount")
+fun_boxplot(freMTPL_filtered, "CarAge", "ClaimAmount")
+fun_boxplot(freMTPL_filtered, "Power", "ClaimAmount")
+fun_boxplot(freMTPL_filtered, "DriverAge", "ClaimAmount")
 
 
 ### Modele Gamma lien log ----
-# freMTPL <- freMTPL %>% 
-#   mutate(ClaimAmount = ifelse(is.na(ClaimAmount), 0, ClaimAmount)) 
-
-fgamma <- glm(ClaimAmount ~ DriverAgeG+LDensity, offset=log(Exposure),
+fgamma <- glm(ClaimAmount ~ DriverAgeG+LDensity,
               family=Gamma("log"), data=freMTPL_filtered)
 summary(fgamma)
-
-plot(fitted(fgamma), resid(fgamma), 
-     ylab="Residuals", xlab="fitted",
-     title = "Gamma lien log") 
+plotgroupresiduals(fgamma, m=1, trim = F) #, trim = F, m =1
 
 ### Modele Inverse gaussienne lien log ----
-fig_log <- glm(ClaimAmount ~ DriverAgeG+LDensity, offset=log(Exposure),
-              family=gaussian("log"), data=freMTPL)
+fig_log <- glm(ClaimAmount ~ DriverAgeG+LDensity,
+              family=inverse.gaussian("log"), data=freMTPL_filtered, maxit = 50)
 summary(fig_log)
+plotgroupresiduals(fig_log, m=1, trim = F) #, trim = F, m =1
 
+### Modele log gamma ----
+flgamma <- glm(LClaimAmount ~ DriverAgeG+LDensity,
+               family=Gamma("identity"), data=freMTPL_filtered)
+summary(flgamma)
+plotgroupresiduals(flgamma, m=1, trim = F) #, trim = F, m =1
 
