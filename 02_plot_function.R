8c2d484e515c709685d695761ff9a385941e5053
emaucuer <96245397+emaucuer@users.noreply.github.com>
2022-01-04 20:34:10 +0100

    Change fun barplot to plot graph side by side

--- 02_plot_function.R
+++ 02_plot_function.R
@@ -5,44 +5,37 @@ my_colors <- c(RColorBrewer::brewer.pal(6, "Blues")[1:6])
 
 fun_plot <- function(data,var1,var2){
 
-              data_plot1 <<- data %>% 
-                group_by(across(var1),across(var2)) %>% 
-                summarise(n = n()) %>% 
-                ungroup()
-              
-              data_plot2 <<- data %>% 
-                group_by(across(var1)) %>% 
-                summarise(n_var1 = n()) %>% 
-                ungroup()
-              
-              print(as.character(as.name(var1)))
-              data_plot <<- merge(data_plot1, data_plot2, by = var1)
-              
-              data_plot$freq = data_plot$n/data_plot$n_var1
-              
-              data_plot <<- data_plot
+  data_plot1 <- data %>% 
+    group_by(across(var1),across(var2)) %>% 
+    summarise(n = n(), .groups = "keep") 
+  data_plot2 <- data %>% 
+    group_by(across(var1)) %>% 
+    summarise(n_var1 = n()) 
   
-              plot <- plot_ly(data_plot) %>%
-                add_trace(x = as.formula(paste0("~", var1)), y =~ n, 
-                          type = 'bar', color = as.formula(paste0("~","as.factor(", var2,")")),
-                          colors = "Reds") %>% 
-                layout(barmode = 'stack',
-                       yaxis = list(title = "Absolute frequency"),
-                       title = paste0(var2," by ", var1))
-              print(plot)
-              
-              plot_100 <- plot_ly(data_plot) %>%
-                add_trace(x = as.formula(paste0("~", var1)), y =~ freq, 
-                          type = 'bar', color = as.formula(paste0("~","as.factor(", var2,")")), 
-                          colors = "Reds") %>% 
-                layout(barmode = 'stack',
-                       yaxis = list(title = "proportion"),
-                       title = paste0(var2," by ", var1))
-              print(plot_100)
-              
-              
+  data_plot <- merge(data_plot1, data_plot2, by = var1)
+  
+  data_plot$freq = data_plot$n/data_plot$n_var1
+  
+  p1 <- plot_ly(data_plot, showlegend = F) %>%
+    add_trace(x = as.formula(paste0("~", var1)), y =~ n, 
+              type = 'bar', color = as.formula(paste0("~","as.factor(", var2,")")),
+              colors = "Reds") %>% 
+    layout(barmode = 'stack',
+           yaxis = list(title = "Absolute frequency"),
+           title = paste0(var2," by ", var1))
+
+  p2 <- plot_ly(data_plot) %>%
+    add_trace(x = as.formula(paste0("~", var1)), y =~ freq, 
+              type = 'bar', color = as.formula(paste0("~","as.factor(", var2,")")), 
+              colors = "Reds") %>% 
+    layout(barmode = 'stack',
+           yaxis = list(title = "proportion"),
+           title = paste0(var2," by ", var1))
+
+  
+  subplot(p1, p2, nrows = 1, margin = 0.1)
 }
-# fun_plot(data = freMTPLfreq,var1 = "CarAge",var2 = "ClaimNb")
+# fun_plot(data = freMTPLfreq,var1 = "CarAge",var2 = "ClaimNb", T)
 # fun_plot(data = freMTPLfreq,var1 = "DriverAge",var2 = "ClaimNb")
 
 
@@ -58,9 +51,9 @@ fun_boxplot <- function(data, var1, var2){
               colors = "grey") %>%
     layout(boxmode = "group",
            xaxis = list(title = var1),
-           yaxis = list(title = var2, range = c(0, 0.8*m)),
+           yaxis = list(title = var2),
            title = paste0(var2," by ", var1))
-  print(fig)
+  fig
 }
 #fun_boxplot(freMTPL_filtered, "DriverAge", "ClaimAmount")
 
