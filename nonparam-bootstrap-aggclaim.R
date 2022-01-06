5ddb0cd8e37920c5f4ba8b532fdb2e9462af977a
Benjamin Patra <45102364+benjaminpatra@users.noreply.github.com>
2022-01-06 21:24:07 +0100

    code supplementaire prof

---
+++ nonparam-bootstrap-aggclaim.R
@@ -0,0 +1,30 @@
+
+#### simulation de charge sinistre portefeuille non parametrique ####
+rclaimnb <- function(n, mydata, var="ClaimNb")
+{
+  nbrow <- NROW(mydata)
+  rclaimnb1 <- function()
+  {
+    rrow <- sample.int(nbrow, replace=TRUE)
+    sum(mydata[rrow, var])
+  }
+  replicate(n, rclaimnb1())
+}
+rclaimnb(6, freMTPLfreq)
+
+rclaimagg <- function(n, mydatanb, mydataamount, varnb="ClaimNb", varamount="ClaimAmount")
+{
+  nbrow <- NROW(mydataamount)
+  ragg1 <- function(nbclaim)
+  {
+    rrow <- sample(nbrow, size=nbclaim, replace=TRUE)
+    sum(mydataamount[rrow, varamount])
+  }
+  rnb <- rclaimnb(n, mydatanb, var=varnb)
+  sapply(rnb, ragg1)
+}
+#essai
+rclaimagg(6, freMTPLfreq, freMTPLsev)
+#10 000 simu
+rbootclaimagg <- rclaimagg(1e4, freMTPLfreq, freMTPLsev)
+
